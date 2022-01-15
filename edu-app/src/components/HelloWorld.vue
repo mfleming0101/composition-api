2ab72f88b513dc8d83d362df61b65b1e87246d3c
bot <81874346+mfleming0101@users.noreply.github.com>
2022-01-15 14:16:27 -0600

    initialize spacesLeft computed property

--- edu-app/src/components/HelloWorld.vue
+++ edu-app/src/components/HelloWorld.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>Capacity: {{ capacity }}</div>
-  <button @click="increaseCapacity">Increase Capacity</button>
+  <div>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
+    <button @click="increaseCapacity">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">{{ name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 export default {
   setup() {
     const capacity = ref(3)
+    const attending = ref(['Tim', 'Bob', 'Joe'])
+
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length
+    })
 
     function increaseCapacity() {
       capacity.value++
     }
 
-    return { capacity, increaseCapacity }
+    return { capacity, attending, spacesLeft, increaseCapacity }
   }
 }
 </script>
